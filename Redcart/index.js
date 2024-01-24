93a67dd98a4bac09227ce3c4220c2e50acdbd5f1
Abineshvaran <90787443+Abineshvaran@users.noreply.github.com>
2024-01-24 16:38:22 +0530

    Add files via upload

---
+++ Redcart/index.js
@@ -0,0 +1,13 @@
+var sidenav = document.querySelector(".side-navbar")
+
+
+function showNavbar() {
+
+    sidenav.style.left = "0"
+
+}
+function closeNavbar() {
+
+    sidenav.style.left = "-60%"
+
+}
